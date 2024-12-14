da519406ac38ce925e5b3dc6683d391337ff9800
Udeshya Sharma <udeshyasharma17@gmail.com>
2024-12-15 01:32:24 +0530

    deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { useState } from "react";
-// import Navbar from "./components/Navbar/navbar";
 import HomePage from "./components/HomePage/home";
 import LoginPage from "./components/LoginPage/login";
 import RegisterPage from "./components/RegisterPage/register";
 
 
 export const config = {
-  endpoint: `http://localhost:8082`,
+  endpoint: `https://mentorship-matching-platform-wrri.onrender.com`,
 };
 
 function App() {
